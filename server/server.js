0a4d06035f8f9d63383b03794ebf7a036dcd69c1
cspranam <cspranam@gmail.com>
2017-09-12 21:58:20 +0100

    todo rest api

---
+++ server/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/Mongoose.js');
+var {Todo} = require('./models/Todos.js');
+var {User} = require('./models/Users.js');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req,res) => {
+	console.log(req.body);
+	var todo = new Todo({
+		text:req.body.text
+	});
+	todo.save().then((doc) => {
+		res.status(201).send(doc);
+	}, (err) => {
+		res.status(400).send(err);
+	});
+});
+
+app.listen(3000, ()=> {
+	console.log('Started on port 3000');
+})
+
+
+
